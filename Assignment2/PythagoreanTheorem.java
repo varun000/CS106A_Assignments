502276db38ade71f79a6568d8c8890fcb012521e
Sai Varun Vukkem <varun.vukkem@gmail.com>
2015-12-16 20:32:11 +0000

    Completed PythagoreanTheorem.java

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -1,7 +1,7 @@
 /*
  * File: PythagoreanTheorem.java
- * Name: 
- * Section Leader: 
+ * Name: Varun Vukkem
+ * Section Leader: Varun Vukkem
  * -----------------------------
  * This file is the starter file for the PythagoreanTheorem problem.
  */
@@ -10,6 +10,17 @@ import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		println ("Enter values to compute the Pythagorean theorem.");
+		
+		double a = readDouble ("a: ");
+		double b = readDouble ("b: ");
+		
+		/*
+		 * a square + b square = c square
+		 * c = square root (a square + b square)
+		 */
+		double c = Math.sqrt((a * a) + (b * b));
+		
+		println ("c = " + c);
 	}
 }
